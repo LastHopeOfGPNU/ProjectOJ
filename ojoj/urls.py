0a086d53d20ed9eff452513f6a419f12c02e81b3
lyx <28721054@qq.com>
2018-07-13 14:41:23 +0800

    Very first JSON response!

--- ojoj/urls.py
+++ ojoj/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
+from django.conf.urls import url
 
 app_name = 'ojoj'
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'users', views.users),
 ]
--- ojoj/views.py
+++ ojoj/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .models import Users
+from.serializers import UserSerializer
+
+
 
 def index(request):
     return HttpResponse("Hi, index.")
+
+# 标记可被跨站访问
+@csrf_exempt
+def users(request):
+    if request.method == 'GET':
+        user = Users.objects.get(user_id='2015034743003')
+        serializer = UserSerializer(user)
+        return JsonResponse(serializer.data, safe=False)
 
